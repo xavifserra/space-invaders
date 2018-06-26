ec89d1b23a8be5d34948d8e05a2e7a6670ab069e
xavifserra <38772006+xavifserra@users.noreply.github.com>
2018-06-26 19:27:54 +0200

    preparando sprites

--- js/keyControl.js
+++ js/keyControl.js
@@ -11,8 +11,22 @@ KeyControl.prototype._assignEventsToKeys = function () {
     this.keysBuffer[e.code] = true;
     console.log(e.code);
   }.bind(this));
+
   window.addEventListener('keyup', function (e) {
     this.keysBuffer[e.code] = false;
     console.log(e.code);
+  }.bind(this));
+//control GAMEPAD
+  window.addEventListener("gamepadconnected", function(e) {
+    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+    e.gamepad.index, e.gamepad.id,
+    e.gamepad.buttons.length, e.gamepad.axes.length);
+    //gamepadAPI.connect;
+  }.bind(this));
+
+  window.addEventListener("gamepaddisconnected", function(e) {
+    console.log("Gamepad disconnected from index %d: %s",
+      e.gamepad.index, e.gamepad.id);
+      //gamepadAPI.disconnect;
   }.bind(this));
 };
